7af507c0a3429d74ff7c78bf0ff714a5240dd7df
Bader <btabeche23@gmail.com>
2021-03-09 16:02:43 +0100

    added subreddit slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import postsReducer from './slices/postsSlice'
+import subPostsReducer from './slices/subPostsSlice'
 
 export default configureStore({
   reducer: {
    posts : postsReducer,
+   subredditPosts: subPostsReducer
   },
   middleware : getDefaultMiddleware({
     immutableCheck: false,
