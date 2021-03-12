896147956192ea6d39b2871bde806bd8b500a057
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-03-12 21:12:27 +0100

    state only keeps useful data

--- src/redux/slices/postsSlice.js
+++ src/redux/slices/postsSlice.js
@@ -4,7 +4,7 @@ import {
   createEntityAdapter,
 } from "@reduxjs/toolkit";
 
-import axios from 'axios'
+import axios from "axios";
 
 const postsAdapter = createEntityAdapter();
 
@@ -13,21 +13,43 @@ const initialState = postsAdapter.getInitialState({
   error: null,
 });
 
-export const fetchPosts = createAsyncThunk("posts/fetchPosts", async (subreddit) => {
-  let sub 
-  if (subreddit){
-    sub = 'r/'+subreddit
-  } else{
-    sub = 'hot'
-  }
-  const response = await axios.get('https://www.reddit.com/'+sub+'/.json')
-  .then((response) => response.data.data.children);
-    let list = [];
-    for (let num in response) {
-      list.push(response[num].data)
+export const fetchPosts = createAsyncThunk(
+  "posts/fetchPosts",
+  async (subreddit) => {
+    let sub;
+    if (subreddit) {
+      sub = "r/" + subreddit;
+    } else {
+      sub = "hot";
     }
-  return list;
-});
+    const response = await axios
+      .get("https://www.reddit.com/" + sub + "/.json")
+      .then((response) => response.data.data.children);
+    let list = [];
+    for (let x in response) {
+      let post = {
+        id: response[x].data.id,
+        author: response[x].data.author_fullname,
+        is_video: response[x].data.is_video,
+        media: response[x].data.media,
+        post_id: response[x].data.name,
+        over_18: response[x].data.over_18,
+        link: response[x].data.permalink,
+        selftext: response[x].data.selftext,
+        subreddit: response[x].data.subreddit,
+        subreddit_id: response[x].data.subreddit_id,
+        thumbnail: response[x].data.thumbnail,
+        title: response[x].data.title,
+        ups: response[x].data.ups,
+        url: response[x].data.url,
+        is_gallery: response[x].data.is_gallery,
+      };
+      list.push(post);
+    }
+
+    return list;
+  }
+);
 
 const postsSlice = createSlice({
   name: "posts",
@@ -35,23 +57,23 @@ const postsSlice = createSlice({
   reducers: {},
   extraReducers: {
     [fetchPosts.pending]: (state, action) => {
-      state.status = 'loading'
+      state.status = "loading";
     },
     [fetchPosts.fulfilled]: (state, action) => {
-      state.status = 'succeeded'
-      postsAdapter.upsertMany(state, action.payload)
+      state.status = "succeeded";
+      postsAdapter.upsertMany(state, action.payload);
     },
     [fetchPosts.rejected]: (state, action) => {
-      state.status = 'failed'
-      state.error = action.error.message
+      state.status = "failed";
+      state.error = action.error.message;
     },
-  }
+  },
 });
 
 export const {
   selectAll: selectAllPosts,
   selectById: selectPostById,
-  selectIds: selectPostIds
-} = postsAdapter.getSelectors(state => state.posts)
+  selectIds: selectPostIds,
+} = postsAdapter.getSelectors((state) => state.posts);
 
 export default postsSlice.reducer;
