47aae7c94b04187730c2788604b438517adf7c15
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-03-12 22:56:35 +0100

    added link to authorize

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,11 +1,17 @@
 const NavBar = () => {
+const TYPE = "code";
+const RANDOM_STRING = "hdiudou9083jkdsa";
+const URI = "http://localhost:3000/callback/";
+const DURATION = "permanent";
+const SCOPE_STRING = "identity, edit, flair, history, modconfig, modflair, modlog, modposts, modwiki, mysubreddits, privatemessages, read, report, save, submit, subscribe, vote, wikiedit, wikiread";
+const url = `https://www.reddit.com/api/v1/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=${TYPE}&state=${RANDOM_STRING}&redirect_uri=${URI}&duration=${DURATION}&scope=${SCOPE_STRING}`;
     return ( 
         <div className="home-nav">
           <a className="logo" href="/">
             MinimalReddit
           </a>
           <textarea className="searcher" name="search" id="search" placeholder="search"></textarea>
-          <button className="login">Login</button>
+          <button className="login"><a href={url}>Login</a></button>
         </div>
     );
 }
