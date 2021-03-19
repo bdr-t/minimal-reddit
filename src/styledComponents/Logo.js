6ca0858190f6e441d44f2034fca758fd1ffb624a
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-03-20 00:03:16 +0100

    removed folder

--- src/styledComponents/Logo.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Logo = styled.a`
-  color: white;
-  font-weight: 700;
-  text-decoration: none;
-  align-self: center;
-  cursor: pointer;
-`;
--- src/styledComponents/NavContainer.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const NavContainer = styled.div`
-    width: 750px;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-`
