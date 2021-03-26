3cecc3dd88d9812d9e5845ee2fd9456a00e86dbd
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-03-26 13:35:26 +0100

    hook to fetch comments

---
+++ src/actions/useComments.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+function useComments(id) {
+    const [comments,setComments] = useState('')
+    const [status, setStatus] = useState('idle')
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            const algo = await fetch(
+              `https://www.reddit.com/comments/${id}.json`
+            )
+              .then((res) => res.json())
+              .then((json) =>
+                json[1].data.children.map((data) => {
+                  const {
+                      author,
+                    author_fullname,
+                    body_html,
+                    created_utc,
+                    depth,
+                    downs,
+                    id,
+                    parent_id,
+                    permalink,
+                    replies,
+                    ups,
+                    body,
+                  } = data.data;
+                  let obj = {
+                      author,
+                    author_fullname,
+                    body_html,
+                    created_utc,
+                    depth,
+                    downs,
+                    id,
+                    parent_id,
+                    permalink,
+                    replies,
+                    ups,
+                    body,
+                  };
+                  return obj;
+                })
+              );
+        
+              setComments(algo)
+              setStatus('succeded')
+          };
+        fetchData()  
+    },[])
+  
+  return {comments, status}
+}
+
+export default useComments
