289c58f84dbf7bac4ec0cd6a7b3e469875482097
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-03-28 22:45:34 +0200

    changed layout style

--- src/styledComponents.js
+++ src/styledComponents.js
@@ -8,14 +8,58 @@ import {
   HiOutlineSave
 } from "react-icons/hi";
 
+import {Link} from 'react-router-dom'
+
+
+const color = {
+  colorButtonText: '#fff',
+colorPrimary100: '#dee3ea',
+colorPrimary200: '#b2bdcd',
+colorPrimary300: '#5d7290',
+colorPrimary600: '#323d4d',
+colorPrimary700: '#242c37',
+colorPrimary800: '#151a21',
+colorPrimary900: '#0b0e11',
+colorSecondaryWashedOut: '#879eed',
+colorSecondary: '#5575e7',
+colorAccentGlow: 'rgba(253,77,77,0.3)',
+colorAccent: '#fd4d4d',
+colorAccentHover: '#fd6868',
+colorAccentDisabled: '#f5bfbf',
+
+}
+
+export const AppDiv = styled.div`
+background-color: ${color.colorPrimary800};
+height: 100%;
+`
+
+export const Button = styled.button`
+    padding: 10px;
+    background-color: lighgrey;
+    border-radius: 8px;
+    outline: none;
+    border: none;
+    margin: 1em;
+    background-color: ${color.colorAccent};
+    &:hover{
+      background-color: ${color.colorAccentHover}
+    }
+`
+export const Linked = styled(Link)`
+text-decoration: none;
+color: inherit;
+
+`
+
 export const ArrowDown = styled(TiArrowDownThick)`
   align-self: center;
   width: 20px;
   height: auto;
-  color: ${props => props.focus ? "black" : "red"};
+  color: ${props => props.focus ? color.colorPrimary200: color.colorAccent};
   cursor: pointer;
   &:hover {
-    color: red;
+    color: ${color.colorAccentHover};
   }
 `;
 
@@ -24,10 +68,10 @@ export const SaveIcon = styled(HiOutlineSave)`
   padding-left: 1em;
   width: 20px;
   height: auto;
-  color: ${props => props.saved === 1 ? "green" : "black"};
+  color: ${props => props.saved === 1 ? color.colorAccent : color.colorPrimary200};
   cursor: pointer;
   &:hover {
-    color: green;
+    color: ${color.colorAccentHover};
   }
 `;
 
@@ -37,16 +81,16 @@ export const ArrowUp = styled(TiArrowUpThick)`
   width: 20px;
   height: auto;
   cursor: pointer;
-  color: ${props => props.focus === 1 ? "blue" : "black"};
+  color: ${props => props.focus === 1 ? color.colorAccent : color.colorPrimary200};
   &:hover {
-    color: blue;
+    color: ${color.colorAccentHover};
   }
 `;
 
-export const SubReddit = styled.a`
+export const SubReddit = styled.div`
   font-family: Segoe UI SemiBold;
   font-size: 15px;
-  color: #65676b;
+  color: ${color.colorPrimary300};
   text-decoration: none;
   cursor: pointer;
   align-self: center;
@@ -63,7 +107,7 @@ export const Container = styled.div`
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
 
   /* border: 1px solid black; */
-  background-color: white;
+  background-color: ${color.colorPrimary700};
   margin-bottom: 1em;
   display: hidden;
   @media (max-width: 768px) {
@@ -76,7 +120,7 @@ export const Container = styled.div`
 export const Title = styled.h2`
   font-family: Segoe UI Semibold;
   font-size: 15px;
-  color: black;
+  color: white;
   padding: 0.75em 1em 0 1em;
   /* border-bottom: 1px solid black; */
 `;
@@ -120,9 +164,9 @@ export const ContentImage = styled.img`
 `;
 
 export const SubTitle = styled.div`
+color: ${color.colorPrimary300};
   font-family: Segoe UI Light;
   font-size: 13px;
-  color: #65676b;
 `;
 
 export const Video = styled(ReactHlsPlayer)`
@@ -131,6 +175,7 @@ export const Video = styled(ReactHlsPlayer)`
   background-color: black;
 `;
 export const Text = styled.div`
+color: white;
   padding: 4px 1em 1em 1em;
   font-family: Segoe UI;
   font-size: 15px;
@@ -148,17 +193,21 @@ export const Enlace = styled.a`
 export const UpvotesNum = styled.p`
   font-family: Segoe UI SemiBold;
   font-size: 15px;
-  color: #65676b;
+  color:  ${color.colorPrimary300};;
   margin:0;
 `;
 
 export const Comments = styled(UpvotesNum)`
   margin: auto;
+  color:  ${color.colorPrimary300};
+  font-family: Segoe UI SemiBold;
 `;
 
 export const Input = styled.input`
   border-radius: 8px;
+  background-color:  ${color.colorPrimary600};;
   height: 30px;
+  width: 680px;
   align-self: center;
   outline: none;
   border: none;
@@ -184,7 +233,8 @@ export const Login = styled.a`
 `;
 
 export const Logo = styled.a`
-  color: white;
+font-size: 20px;
+  color: ${color.colorAccent};
   font-weight: 700;
   text-decoration: none;
   align-self: center;
@@ -193,17 +243,18 @@ export const Logo = styled.a`
 `;
 
 export const Nav = styled.div`
+
   width: 100%;
   display: flex;
   margin: 0;
   height: 55px;
   justify-content: center;
   margin-bottom: 2em;
-  background-color: black;
+  background-color: ${color.colorPrimary800};
 `;
 
 export const NavContainer = styled.div`
-  width: 750px;
+  max-width: 1200px;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
 `;
