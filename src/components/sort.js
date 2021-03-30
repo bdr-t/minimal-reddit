b45d1f143bd0c31de4a250a22550ccbd98b928b5
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-03-30 22:27:36 +0200

    clean-up

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,5 +1,4 @@
-import styled from 'styled-components'
-import {Link, useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom'
 import {Button, Linked} from '../styledComponents'
 
 
