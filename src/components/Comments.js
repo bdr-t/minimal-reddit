ae326a24a085c38b250f41d03f1a4dbcdbfdbf30
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-03 22:36:41 +0200

    fixed onScroll

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -5,14 +5,21 @@ import {Container} from "../styledComponents"
 const Comments = ({postId}) => {
 
 
-    const {comments, status} = useComments(postId)
+    const {comments, status} = useComments(`https://www.reddit.com/comments/${postId}.json`)
 
     
     let content = 'loading'
     if (status === 'succeded'){
-        console.log(comments)
-        content = comments.map(x=> <Comment author={x.author} id={x.id} body={x.body_html} replies={x.replies} created={x.created_utc}></Comment> )
+        content = comments.map(x=> <Comment author={x.author} id={x.id} body={x.body_html} replies={x.replies} created={x.created_utc} permalink={x.permalink}></Comment> )
     }
+    window.onscroll = function (ev) {
+        if (
+          window.innerHeight + window.pageYOffset >=
+            document.body.offsetHeight * 0.9
+        ) {
+          const doNothing = true
+        }
+      };
 
     return ( 
         <Container >
