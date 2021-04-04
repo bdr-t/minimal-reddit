244d237bd616730e95932f63eb05b95b28dc3b24
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-05 01:13:54 +0200

    fixed best bug

--- src/App.js
+++ src/App.js
@@ -37,9 +37,11 @@ function App() {
           <Route exact path="/r/:subReddit/top" component={PostList} />
           <Route exact path="/r/:subReddit/new" component={PostList} />
           <Route exact path="/r/:subReddit/rising" component={PostList} />
+          <Route exact path="/r/:subReddit/best" component={PostList} />
           <Route exact path="/hot" component={PostList} />
           <Route exact path="/new" component={PostList} />
           <Route exact path="/top" component={PostList} />
+          <Route exact path="/best" component={PostList} />
           <Route exact path="/rising" component={PostList} />
           <Route exact path="/callback" component={Callback} />
           <Route exact path="/post/:id" component={PostPage} />
