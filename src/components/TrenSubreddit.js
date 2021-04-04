97cc399dd0295d4ca97bd498ac0e7dc361d8bc78
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-04 22:39:53 +0200

    styled TrenSubreddit component

--- src/components/TrenSubreddit.js
+++ src/components/TrenSubreddit.js
@@ -1,26 +1,41 @@
-import {useEffect, useState} from 'react'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const TrenSubreddit = ({name}) => {
-    const [subReddit, setSubreddit] = useState()
+const TrenSubreddit = ({ name }) => {
+  const [subReddit, setSubreddit] = useState();
+  const [icon, setIcon] = useState("https://i.imgur.com/Td33Ac4.png");
 
-    useEffect(()=>{
-        const getSubreddit = async() => {
-            const data = await axios.get(`https://www.reddit.com/r/${name}/about.json`).then(res => res.data.data)
-            console.log(data)
-            setSubreddit(data)
+  useEffect(() => {
+    const getSubreddit = async () => {
+      const data = await axios
+        .get(`https://www.reddit.com/r/${name}/about.json`)
+        .then((res) => res.data.data);
+      console.log(data);
+      setSubreddit(data);
+      if (data) {
+        let url = data.community_icon;
+        if (url !== "") {
+          const remove = url.split("?").pop();
+          const icon = url.replace(remove, "");
+          setIcon(icon);
         }
-        getSubreddit()
-    },[])
+      }
+    };
+    getSubreddit();
+  }, []);
 
+  
 
-    return (
-        <div>
-
-            <p>{subReddit && subReddit.display_name_prefixed}</p>
-            <p>{subReddit && subReddit.subscribers}</p>
-        </div>
-    );
-}
+  return (
+    <div style={{display:'flex', gap:'1em', padding:'0.5em 1em'}}>
+      <img src={icon} style={{borderRadius: '50%'}} width="40px" alt="" />
+      <div className="a">
+        <p>{subReddit && subReddit.display_name_prefixed}</p>
+        <p>{subReddit && subReddit.subscribers}</p>
+      </div>
+      <p style={{marginLeft: 'auto', borderRadius: '20px', padding:'0.5em 1em', alignSelf:'center', backgroundColor: 'red', cursor:'pointer'}}>Join</p>
+    </div>
+  );
+};
 
 export default TrenSubreddit;
