c622e1996627cf0557d70c437971b668e99231ff
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-04 13:21:25 +0200

    can show replies now

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,83 +1,121 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import axios from "axios";
-import { Text} from "../styledComponents";
+import { Text } from "../styledComponents";
 import TimeAgo from "./TimeAgo";
 import parseHtml from "../actions/parseHTML";
 import useComments from "../actions/useComments";
+import {CommentContainer} from '../styledComponents'
 
-const Comment = ({ author, id, body, replies, created, permalink }) => {
-  const [icon, setIcon] = useState(
-    "https://www.redditstatic.com/avatars/avatar_default_05_7193FF.png"
-  );
+const Comment = ({
+  fullComment,
+  author,
+  id,
+  body,
+  replies,
+  created,
+  permalink,
+}) => {
+  const File = ({ body, replies, author } = data) => {
+    const [icon, setIcon] = useState(
+      "https://www.redditstatic.com/avatars/avatar_default_05_7193FF.png"
+    );
 
-  const [showReplies, setShowReplies] = useState(false)
+    const [showChildren, setShowChildren] = useState(false);
+    const handleClick = useCallback(() => {
+      setShowChildren(!showChildren);
+    }, [showChildren, setShowChildren]);
 
-  useEffect(() => {
-    async function getIcon() {
-      const url = await axios(
-        `https://www.reddit.com/user/${author}/about/.json`
-      ).then((res) => res.data.data.icon_img);
-      if (url.endsWith("png")) {
-        setIcon(url);
-      } else {
-        const remove = url.split("?").pop();
-        const icon = url.replace(remove, "");
-        setIcon(icon);
-      }
+    let children;
+    if (replies) {
+      children = replies.data.children;
     }
-    getIcon();
-  });
+
+    useEffect(() => {
+      async function getIcon() {
+        const url = await axios(
+          `https://www.reddit.com/user/${author}/about/.json`
+        ).then((res) => res.data.data.icon_img);
+        if (url.endsWith("png")) {
+          setIcon(url);
+        } else {
+          const remove = url.split("?").pop();
+          const icon = url.replace(remove, "");
+          setIcon(icon);
+        }
+      }
+      getIcon();
+    });
+
+    return (
+      <CommentContainer>
+        <div>
+          <div
+            className="flex"
+            style={{
+              display: "flex",
+              gap: "0.5em",
+              padding: "0.5em 1em 0.5em 1em",
+            }}
+          >
+            <div
+              className="img"
+              style={{ width: "35px", display: "grid", placeContent: "center" }}
+            >
+              <img
+                src={icon}
+                width="30px"
+                height="30px"
+                style={{ borderRadius: "50%", margin: "auto" }}
+                alt=""
+              />
+            </div>
+
+            <div className="flex-author">{author}</div>
+            <TimeAgo
+              needsPadding={true}
+              created={created}
+              author={author}
+            />
+          </div>
+          {body}
+          {replies && (
+            <p
+              onClick={handleClick}
+              style={{
+                cursor: "pointer",
+                fontFamily: "Segoe UI SemiBold",
+                color: "#65676b",
+                textAlign: "center",
+                fontSize: "15px",
+                paddingBottom: "0.5em",
+              }}
+            >
+              {replies.data.children.length} replies
+            </p>
+          )}
+        </div>
+        <div
+          style={{
+            position: "relative",
+            display: "flex",
+            flexDirection: "column",
+            left: 25,
+            borderLeft: "1px solid",
+            paddingLeft: 15,
+          }}
+        >
+          {showChildren &&
+            (children ?? []).map((data) => <File {...data.data} />)}
+        </div>
+      </CommentContainer>
+    );
+  };
 
   body = <Text>{parseHtml(body)}</Text>;
 
-  let content = 'loading'
+  let data = fullComment;
 
-  return (
-    <div style={{ borderBottom: "1px solid #E9EAED" }}>
-      <div
-        className="flex"
-        style={{
-          display: "flex",
-          gap: "0.5em",
-          padding: "0.5em 1em 0.5em 1em",
-        }}
-      >
-        <div className="img" style={{width: '35px', display:'grid', placeContent:'center'}}>
-          <img
-            src={icon}
-            width="30px"
-            height="30px"
-            style={{ borderRadius: "50%", margin: "auto" }}
-            alt=""
-          />
-        </div>
-
-        <div className="flex-author">
-          {author}
-          <TimeAgo created={created} author={author}/>
-        </div>
-      </div>
-      {body}
-      {replies && (
-        <p
-        onClick={()=> setShowReplies(!showReplies)}
-          style={{
-            cursor:'pointer',
-            fontFamily: "Segoe UI SemiBold",
-            color: "#65676b",
-            textAlign: "center",
-            fontSize: "15px",
-            paddingBottom: "0.5em",
-          }}
-        >
-          {replies.data.children.length} replies
-          <div>
-            {showReplies && content}
-          </div>
-        </p>
-      )}
-    </div>
-  );
+  return <File {...data} />;
 };
 
 export default Comment;
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -10,7 +10,7 @@ const Comments = ({postId}) => {
     
     let content = 'loading'
     if (status === 'succeded'){
-        content = comments.map(x=> <Comment author={x.author} id={x.id} body={x.body_html} replies={x.replies} created={x.created_utc} permalink={x.permalink}></Comment> )
+        content = comments.map(x=> <Comment fullComment={x} author={x.author} id={x.id} body={x.body_html} replies={x.replies} created={x.created_utc} permalink={x.permalink}></Comment> )
     }
     window.onscroll = function (ev) {
         if (
@@ -22,7 +22,7 @@ const Comments = ({postId}) => {
       };
 
     return ( 
-        <Container >
+        <Container style={{overflow:'hidden'}} >
             {content}
         </Container>
         
