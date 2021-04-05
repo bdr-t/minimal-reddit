c62bfde69266c7f43a148ffdcfdd3544657eb47a
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-06 01:25:18 +0200

    custom hook to get subreddit data

---
+++ src/actions/useFetchSubInfo.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function useFetchSubInfo(link, token) {
+  const [data, setData] = useState(null);
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+      console.log('algi')
+    const fetchData = async () => {
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+      const response = await axios
+        .get(link, config)
+        .then((response) => response.data.data);
+        console.log(response)
+      let list = {
+        icon: response.icon_img,
+        accounts_active: response.accounts_active,
+        banner: response.banner_img,
+        description: response.public_description_html,
+        subscribers: response.subscribers,
+        title: response.title,
+        user_is_subscriber: response.user_is_subscriber,
+        community_icon: response.community_icon,
+        banner_background_image: response.banner_background_image,
+        name: response.display_name_prefixed,
+      };
+
+      setData(list);
+      setStatus("succeeded");
+      setError(false);
+    };
+    fetchData();
+  }, [link]);
+
+  return { data, status, error };
+}
+
+export default useFetchSubInfo;
