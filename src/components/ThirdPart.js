fd9c973517a7e3ac949d95b9cacfc2aee80bf1ac
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-06 01:27:40 +0200

    added subreddit component

--- src/components/ThirdPart.js
+++ src/components/ThirdPart.js
@@ -1,10 +1,17 @@
 import {Input} from '../styledComponents'
 import TrendingCommunities from './TrendingCommunities'
+import {useParams} from 'react-router-dom'
+import SubRedditInfo from './SubRedditInfo'
 const ThirdPart = () => {
+    const {subReddit} = useParams()
+    
+
     return ( 
         <div style={{display: 'grid', gridTemplateRows:'60px auto', gap:'50px', height: '100vh', position:'sticky', top:'0'}}>
             <Input/>
-            <TrendingCommunities/>
+            {!subReddit && <TrendingCommunities/>}
+            {subReddit && <SubRedditInfo/>}
+            
         </div>
      );
 }
