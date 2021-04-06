382fbe21742b5f5b1c6eea05bb2687eebc11d149
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-06 21:07:53 +0200

    added function to fetch username

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,6 +3,8 @@ import LoggedIn from "./LoggedIn";
 import NotLoggedIn from "./NotLoggedIn";
 import { Section } from "../styledComponents";
 import LogoNav from "./LogoNav";
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 import Sort from "./sort";
 import TrendingCommunities from "./TrendingCommunities";
@@ -12,12 +14,30 @@ const PostList = ({ match }) => {
   const authorization = useSelector(
     (state) => state.authorization.authorization
   );
+  const token = useSelector((state) => state.authorization.token);
+    const [userName, setUserName] = useState();
+    useEffect(() => {
+      if (token) {
+        async function getIcon() {
+          const config = {
+            headers: { Authorization: `Bearer ${token}` },
+          };
+          const data = await axios(
+            `https://oauth.reddit.com/api/v1/me`,
+            config
+          ).then((res) => res);
+          console.log(data);
+          setUserName(data.data.name);
+        }
+        getIcon();
+      }
+    }, [token]);
   return (
     <Section>
       <LogoNav />
       <div>
         <Sort />
-        {authorization && <LoggedIn match={match} />}
+        {authorization && userName && <LoggedIn match={match} username={userName}/>}
         {!authorization && <NotLoggedIn match={match} />}
       </div>
 
