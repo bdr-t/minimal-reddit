a4e45c20d59ea68053ede5d2e6b4225adaf7c22a
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-06 18:02:19 +0200

    fixed bug- now SubRedditInfo will always render

--- src/components/ThirdPart.js
+++ src/components/ThirdPart.js
@@ -2,15 +2,18 @@ import {Input} from '../styledComponents'
 import TrendingCommunities from './TrendingCommunities'
 import {useParams} from 'react-router-dom'
 import SubRedditInfo from './SubRedditInfo'
+import {useSelector} from 'react-redux'
 const ThirdPart = () => {
     const {subReddit} = useParams()
+    const token = useSelector((state) => state.authorization.token);
+    
     
 
     return ( 
         <div style={{display: 'grid', gridTemplateRows:'60px auto', gap:'50px', height: '100vh', position:'sticky', top:'0'}}>
             <Input/>
             {!subReddit && <TrendingCommunities/>}
-            {subReddit && <SubRedditInfo/>}
+            {subReddit && token && <SubRedditInfo/>}
             
         </div>
      );
