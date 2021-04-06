966096b92f447bc77dc731d0d19c6198ad86b01b
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-04-06 18:25:31 +0200

    created a layer componet that fetches and sends data as props

--- src/components/ThirdPart.js
+++ src/components/ThirdPart.js
@@ -1,8 +1,8 @@
 import {Input} from '../styledComponents'
 import TrendingCommunities from './TrendingCommunities'
 import {useParams} from 'react-router-dom'
-import SubRedditInfo from './SubRedditInfo'
 import {useSelector} from 'react-redux'
+import SubLayer from './SubLayer'
 const ThirdPart = () => {
     const {subReddit} = useParams()
     const token = useSelector((state) => state.authorization.token);
@@ -13,7 +13,7 @@ const ThirdPart = () => {
         <div style={{display: 'grid', gridTemplateRows:'60px auto', gap:'50px', height: '100vh', position:'sticky', top:'0'}}>
             <Input/>
             {!subReddit && <TrendingCommunities/>}
-            {subReddit && token && <SubRedditInfo/>}
+            {subReddit && token && <SubLayer/>}
             
         </div>
      );
