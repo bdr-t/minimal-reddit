f319baf25c394fb109631d4b33bf99e36384eb58
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-05-07 01:48:46 +0200

    automaticly redirects to home after login

--- src/components/CallBack.js
+++ src/components/CallBack.js
@@ -1,22 +1,24 @@
 import {useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import { fetchToken } from '../redux/slices/authorizationSlice';
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
+
 
 
 
 const Callback = () => {
+    let history = useHistory()
     const code = window.location.search.match(/code=([^&]*)/)[1];
     const dispatch = useDispatch()
 
     useEffect(()=>{
         dispatch(fetchToken(code))
-
+        history.push('/')
     },[code, dispatch])
 
     return ( 
         <div>
-            <h1>LOGING IN</h1>
+            <h1>You are now logged in. You will soon be redirected</h1>
             <button >
                 <Link to='/'>HOME</Link>
             </button>
