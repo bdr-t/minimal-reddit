57929a1285aa90d72aabfeb50274b97a991c4fd5
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-05-08 01:37:20 +0200

    Not found component created

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,15 @@
+import {Title, Linked, BtnCreate} from '../styledComponents'
+
 const NotFound = () => {
-    return (  
-        <div style={{background:'wheat', color:'black',height:'100vh'}}>Not found</div>
-    );
-}
- 
+  return (
+    <div style={{ display: 'grid', height:'100vh'}}>
+      <div style={{ placeSelf: 'center' }}>
+        <Title style={{fontSize:'25px', textAlign:'center'}}>Oops! Something is clearly wrong here... </Title>
+        <Title style={{textAlign:'center'}} > The page you are looking for doesn’t exist. (404)</Title>
+        <Linked to='/'><BtnCreate style={{margin:'auto', marginTop:'1em'}}>GO HOME</BtnCreate></Linked>
+      </div>
+    </div>
+  );
+};
+
 export default NotFound;
