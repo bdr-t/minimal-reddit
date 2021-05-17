d57612a65a396ad96600ce15525da390b62e1996
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-05-17 12:11:47 +0200

    scroll to top on subreddits and postPage

--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -1,16 +1,19 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useRef } from "react";
-import Post from "./Post";
-import { useParams } from "react-router-dom";
-import { fetchPosts, remove, selectAllPosts } from "../redux/slices/postsSlice";
-import Notifications from "./Notifications";
-import Messages from "./Messages";
-import MyProfile from "./MyProfile";
-
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import Post from './Post';
+import { useParams } from 'react-router-dom';
+import { fetchPosts, remove, selectAllPosts } from '../redux/slices/postsSlice';
+import Notifications from './Notifications';
+import Messages from './Messages';
+import MyProfile from './MyProfile';
 
 const LoggedIn = ({ match, username, user }) => {
-  const path = match ? match.url : "/best";
+  const path = match ? match.url : '/best';
   const { subReddit } = useParams();
+  console.log('THE MATCH-----------------');
+  console.log(match);
+  console.log(subReddit)
+  
 
   const observer = useRef(path);
 
@@ -24,51 +27,55 @@ const LoggedIn = ({ match, username, user }) => {
   const first = useSelector((state) => state.posts.first);
 
   let link;
-  
-  if (path === "/saved") {
+
+  if (path === '/saved') {
     link = `https://oauth.reddit.com/user/${username}/saved`;
-  } else if (path === "/upvoted") {
+  } else if (path === '/upvoted') {
     link = `https://oauth.reddit.com/user/${username}/upvoted`;
-  } else if (path === "/posts") {
+  } else if (path === '/posts') {
     link = `https://oauth.reddit.com/user/${username}/submitted`;
-  } else if (user){
-    link = `https://oauth.reddit.com/user/${user}/overview`
-  } else if(path.includes('/top/')){
-    if(match.params.subReddit){
-    link = `https://oauth.reddit.com/r/${match.params.subReddit}/top?t=${match.params.id}&limit=10`
-    }else{
+  } else if (user) {
+    link = `https://oauth.reddit.com/user/${user}/overview`;
+  } else if (path.includes('/top/')) {
+    if (match.params.subReddit) {
+      link = `https://oauth.reddit.com/r/${match.params.subReddit}/top?t=${match.params.id}&limit=10`;
+    } else {
       link = `https://oauth.reddit.com/top?t=${match.params.id}&limit=10`;
     }
-    console.log(link)
-  }
-  
-  else {
+    console.log(link);
+  } else {
     link = `https://oauth.reddit.com${path}?limit=10`;
   }
 
-  
-
   useEffect(() => {
     if (!(observer.current === path)) {
-      console.log("removing");
+      console.log('removing');
       dispatch(remove(ids));
     }
-    console.log(observer)
+    console.log(observer);
   }, [path]);
 
   useEffect(() => {
-    setTimeout(()=>{
-      const scrollPosition = sessionStorage.getItem("scrollPosition");
-      window.scrollTo({
-        top: parseInt(scrollPosition),
-        left: 0,
-        behavior: 'smooth'
-      });
-      sessionStorage.setItem("scrollPosition", 0);
-    }, 500)
+    setTimeout(() => {
+      if (subReddit) {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth',
+        });
+      } else {
+        const scrollPosition = sessionStorage.getItem('scrollPosition');
+        window.scrollTo({
+          top: parseInt(scrollPosition),
+          left: 0,
+          behavior: 'smooth',
+        });
+        sessionStorage.setItem('scrollPosition', 0);
+      }
+    }, 500);
 
     if (first) {
-      if (path !== "/me" && path !== "/notifications" && path !== "/messages") {
+      if (path !== '/me' && path !== '/notifications' && path !== '/messages') {
         const config = {
           link,
           token,
@@ -87,11 +94,8 @@ const LoggedIn = ({ match, username, user }) => {
   }, [path, subReddit]);
 
   window.onscroll = function (ev) {
-    if (
-      window.innerHeight + window.pageYOffset >=
-      document.body.offsetHeight * 0.9
-    ) {
-      if (postStatus === "succeeded") {
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight * 0.9) {
+      if (postStatus === 'succeeded') {
         const config = {
           link: `https://oauth.reddit.com${path}?limit=10&after=${after}`,
           token,
@@ -104,21 +108,17 @@ const LoggedIn = ({ match, username, user }) => {
 
   const posts = useSelector(selectAllPosts);
 
-
   let content;
 
-  if (path === "/notifications") {
+  if (path === '/notifications') {
     content = <Notifications />;
-  } else if (path === "/messages") {
+  } else if (path === '/messages') {
     content = <Messages />;
-  } else if (path === "/me") {
+  } else if (path === '/me') {
     content = <MyProfile name={username} />;
   } else {
-    content = posts.map((post) => (
-      <Post key={post.id} postId={post.id} token={token} />
-    ));
+    content = posts.map((post) => <Post key={post.id} postId={post.id} token={token} />);
   }
-
 
   if (error) {
     <div className="err">{error}</div>;
--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,15 +1,15 @@
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import Post from "./Post";
-import Comments from "./Comments";
-import { Section} from "../styledComponents";
-import LogoNav from './LogoNav'
-import ThirdPart from './ThirdPart'
-
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Post from './Post';
+import Comments from './Comments';
+import { Section } from '../styledComponents';
+import LogoNav from './LogoNav';
+import ThirdPart from './ThirdPart';
 
 const PostPage = () => {
   const { id } = useParams();
   const token = useSelector((state) => state.authorization.token);
+  window.scrollTo(0, 0);
 
   return (
     <Section>
@@ -18,7 +18,7 @@ const PostPage = () => {
         <Post postId={id} token={token}></Post>
         <Comments postId={id} />
       </div>
-      <ThirdPart/>
+      <ThirdPart />
     </Section>
   );
 };
