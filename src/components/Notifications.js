9e41c6aa3bea663af6a53da2f331dcd897a5f12d
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-05-17 17:12:02 +0200

    Notification component sends to original post

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,51 +1,57 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Container, MessageBy, Text, Linked } from "../styledComponents";
-import parseHTML from '../actions/parseHTML'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Container, MessageBy, Text, Linked } from '../styledComponents';
+import parseHTML from '../actions/parseHTML';
 
 const Notifications = () => {
-    const token = useSelector((state) => state.authorization.token);
-  const [data, setData] = useState()
+  const token = useSelector((state) => state.authorization.token);
+  const [data, setData] = useState();
+
   useEffect(() => {
     if (token) {
       async function getData() {
-          console.log('algo')
-          const config = {
-            headers: { Authorization: `Bearer ${token}` },
-          };
+        console.log('algo');
+        const config = {
+          headers: { Authorization: `Bearer ${token}` },
+        };
         const response = await axios
-          .get("https://oauth.reddit.com/message/inbox", config)
+          .get('https://oauth.reddit.com/message/inbox', config)
           .then((res) => res.data.data.children);
-        setData(response)
+        setData(response);
       }
-      getData()
+      getData();
     }
   }, [token]);
 
-  let content = 'loading'
+  let content = 'loading';
   // <Linked to={`/r/${post.subreddit}/post/${post.id}`}>
 
-  if(data){
-    content = data.map(x => {
-        if(x.kind === 't1'){
-          const postId = x.data.parent_id.slice(3)
-            return <Container>
-                <MessageBy> <span>{x.data.type === 'post_reply'? 'Post replied' : 'Comment replied'}</span>
-                {x.data.link_title}
-                </MessageBy>
-                <Linked style={{paddingLeft:'15px', fontSize:'13px'}}to={`/u/${x.data.author}`}>by {x.data.author}</Linked>
-                <Text>{parseHTML(x.data.body_html)}</Text>
-                </Container>
-        } 
-        return 
-    })
-}
-return (
-  <div>
-    {content}
-  </div>
-);
-}
- 
+  if (data) {
+    content = data.map((x) => {
+      if (x.kind === 't1') {
+        console.log(x);
+        const postId = x.data.parent_id.slice(3);
+
+        return (
+          <Linked to={`/r/${x.data.subreddit}/post/${postId}`}>
+            <Container>
+              <MessageBy>
+                <span>{x.data.type === 'post_reply' ? 'Post replied' : 'Comment replied'}</span>
+                <Linked>{x.data.link_title}</Linked>
+              </MessageBy>
+              <Linked style={{ paddingLeft: '15px', fontSize: '13px' }} to={`/u/${x.data.author}`}>
+                by {x.data.author}
+              </Linked>
+              <Text>{parseHTML(x.data.body_html)}</Text>
+            </Container>
+          </Linked>
+        );
+      }
+      return;
+    });
+  }
+  return <div>{content}</div>;
+};
+
 export default Notifications;
