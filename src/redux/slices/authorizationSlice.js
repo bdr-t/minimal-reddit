13927b11da49a3b50bf02b3fd52c5cb2c0bf4d12
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-05-18 19:31:54 +0200

    added changeTheme action

--- src/redux/slices/authorizationSlice.js
+++ src/redux/slices/authorizationSlice.js
@@ -1,102 +1,99 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-export const fetchToken = createAsyncThunk(
-  "authorization/fetchToken",
-  async (code) => {
-    let redirectUri = process.env.REACT_APP_REDDIRECT_URI;
-    const encode = btoa(
-      `${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_PASSWORD}`
-    );
-    const response = await fetch("https://www.reddit.com/api/v1/access_token", {
-      method: "POST",
-      body: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}&duration=permanent`,
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: `basic ${encode}`,
-      },
-    })
-      .then((response) => {
-        if(!response.ok){
-          throw new Error('Something went wrong');
-        }
-        return response.json()})
-      .catch((error) => console.log("error", error));
-    window.localStorage.setItem("refreshToken", response.refresh_token);
-    setTimeout(() => {
-      refreshToken(response.refresh_token);
-    }, 3500000);
-    return response;
-  }
-);
-
-export const refreshToken = createAsyncThunk(
-  "authorization/refreshToken",
-  async (refreshToken) => {
-    const encode = btoa(
-      `${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_PASSWORD}`
-    );
-    const response = await fetch("https://www.reddit.com/api/v1/access_token", {
-      method: "POST",
-      body: `grant_type=refresh_token&refresh_token=${refreshToken}&duration=permanent`,
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: `basic ${encode}`,
-      },
-    })
+export const fetchToken = createAsyncThunk('authorization/fetchToken', async (code) => {
+  let redirectUri = process.env.REACT_APP_REDDIRECT_URI;
+  const encode = btoa(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_PASSWORD}`);
+  const response = await fetch('https://www.reddit.com/api/v1/access_token', {
+    method: 'POST',
+    body: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}&duration=permanent`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `basic ${encode}`,
+    },
+  })
     .then((response) => {
-      if(!response.ok){
+      if (!response.ok) {
         throw new Error('Something went wrong');
       }
-      return response.json()})
-      .catch((error) => console.log("error", error));
-    return response;
-  }
-);
+      return response.json();
+    })
+    .catch((error) => console.log('error', error));
+  window.localStorage.setItem('refreshToken', response.refresh_token);
+  setTimeout(() => {
+    refreshToken(response.refresh_token);
+  }, 3500000);
+  return response;
+});
+
+export const refreshToken = createAsyncThunk('authorization/refreshToken', async (refreshToken) => {
+  const encode = btoa(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_PASSWORD}`);
+  const response = await fetch('https://www.reddit.com/api/v1/access_token', {
+    method: 'POST',
+    body: `grant_type=refresh_token&refresh_token=${refreshToken}&duration=permanent`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `basic ${encode}`,
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+      return response.json();
+    })
+    .catch((error) => console.log('error', error));
+  return response;
+});
 
 const authorizationSlice = createSlice({
-  name: "authorization",
+  name: 'authorization',
   initialState: {
-    status: "idle",
+    status: 'idle',
     authorization: false,
     token: null,
     refreshToken: null,
+    theme: 'light',
   },
   reducers: {
-    logOut(state){
+    logOut(state) {
       state.authorization = false;
-    }
+    },
 
+    changeTheme(state) {
+      if (state.theme === 'light') state.theme = 'dark';
+      else state.theme = 'light';
+    },
   },
   extraReducers: {
     [fetchToken.pending]: (state, action) => {
-      state.status = "loading";
+      state.status = 'loading';
     },
     [fetchToken.fulfilled]: (state, action) => {
-      state.status = "succeeded";
+      state.status = 'succeeded';
       state.authorization = true;
       state.token = action.payload.access_token;
       state.refreshToken = action.payload.refresh_token;
     },
     [fetchToken.rejected]: (state, action) => {
-      state.status = "failed";
+      state.status = 'failed';
       state.error = action.error.message;
     },
     [refreshToken.pending]: (state, action) => {
-      state.status = "loading";
+      state.status = 'loading';
     },
     [refreshToken.fulfilled]: (state, action) => {
-      state.status = "succeeded";
+      state.status = 'succeeded';
       state.authorization = true;
       state.token = action.payload.access_token;
       state.refreshToken = action.payload.refresh_token;
     },
     [refreshToken.rejected]: (state, action) => {
-      state.status = "failed";
+      state.status = 'failed';
       state.error = action.error.message;
     },
   },
 });
 
-export const {logOut} = authorizationSlice.actions
+export const { logOut, changeTheme } = authorizationSlice.actions;
 
 export default authorizationSlice.reducer;
