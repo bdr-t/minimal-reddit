137823283ff7093c4a6bbdddabf48d1624674d0e
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-05-18 12:55:14 +0200

    added themes.js

---
+++ src/Themes.js
@@ -0,0 +1,25 @@
+export const darkTheme = {
+  text: 'white',
+  colorPrimary200: '#9CA3AF',
+  colorPrimary300: '#9CA3AF',
+  colorPrimary600: '#6B7280',
+  colorPrimary700: '#374151',
+  colorPrimary800: '#1F2937',
+  colorAccentGlow: 'rgba(253,77,77,0.3)',
+  colorAccent: '#10B981',
+  colorAccentHover: '#fd6868',
+  links: 'lightblue',
+};
+
+export const lightTheme = {
+    text: '#000000',
+    colorPrimary200: '#4B5563',
+    colorPrimary300: '#6B7280',
+    colorPrimary600: '#D1D5DB',
+    colorPrimary700: '#E5E7EB',
+    colorPrimary800: '#F3F4F6',
+    colorAccentGlow: 'rgba(253,77,77,0.3)',
+    colorAccent: '#10B981',
+    colorAccentHover: '#fd6868',
+    links: 'none'
+  };
